f9faeec3c4d8a4e278592c53e999b6a136a0ad34
Jerry Qin <jerry.gn.qin@gmail.com>
2022-02-14 17:09:52 -0600

    random obstacle generation and movement

--- game.py
+++ game.py
@@ -2,7 +2,8 @@
 
 try:
     import sys
-    #import random
+    import random
+    import numpy as np
     #import math
     import os
     #import getopt
@@ -15,12 +16,15 @@ except ImportError, err:
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 data_dir = os.path.join(main_dir, "resources")
+directions = ["up", "left", "right", "down"]
 
 def main():
     # Initialise screen
     pg.init()
     screen = pg.display.set_mode((1500, 1000))
-    pg.display.set_caption('Skillshot dodger')
+    pg.display.set_caption('Skillshot Dodger')
+    obstacles = []
+    # pg.time.set_timer(USEREVENT + 2, random.randrange(2000, 3500))
 
     # Fill background
     background = pg.image.load("resources/background.jpg")
@@ -35,11 +39,38 @@ def main():
     allsprites = pg.sprite.RenderPlain((player))
     clock = pg.time.Clock()
 
+    fireball = Fireball(0)
+    fireball.draw(screen)
+
+    obstacles.append(fireball)
     pg.key.set_repeat(2)
+
+    pg.time.set_timer(USEREVENT + 2, random.randrange(200, 350)) # determines how often we generate a fireball
     # Event loop
     while 1:
         clock.tick(60)
+
+        for obstacle in obstacles:
+
+            # move the obstacle
+            if obstacle.direction == "up":
+                obstacle.y -= obstacle.vel
+            elif obstacle.direction == "down":
+                obstacle.y += obstacle.vel
+            elif obstacle.direction == "left":
+                obstacle.x -= obstacle.vel
+            else:
+                obstacle.x += obstacle.vel
+            obstacle.draw(screen)
+        pg.display.update()  # to render the fireballs each time loop
+
         for event in pg.event.get():
+
+            # generate a new fireball
+            if event.type == USEREVENT+2:
+                r = np.random.choice([0, 1])
+                obstacles.append(Fireball(r))
+
             if event.type == QUIT:
                 return
             elif event.type == pg.KEYDOWN:
@@ -53,7 +84,6 @@ def main():
                     player.move_right()
                 elif event.key == pg.K_LEFT:
                     player.move_left()
-
         allsprites.update()
 
         # Draw Everything
@@ -76,26 +106,60 @@ def load_image(name, colorkey=None, scale=1):
         image.set_colorkey(colorkey, pg.RLEACCEL)
     return image, image.get_rect()
 
+class Fireball(object):
+    # need to define some rational pictures so we can have fireballs in many directions
+    # rotate = [pg.image.load(os.path.join('resources', 'SAW0.PNG')), pg.image.load(os.path.join('resources', 'SAW1.PNG')),
+    #           pg.image.load(os.path.join('resources', 'SAW2.PNG')), pg.image.load(os.path.join('resources', 'SAW3.PNG'))]
+    def __init__(self, i):
+        # generate random location for the fireball on edge of screen
+        if i % 2 == 0:
+            self.x = np.random.randint(0, 1450)
+            self.y = np.random.choice([0, 900])
+        else:
+            self.y = np.random.randint(0, 900)
+            self.x = np.random.choice([0, 1450])
+        self.width = 1
+        self.height = 1
+        self.rotateCount = 0
+        self.vel = np.random.randint(10, 18)
+        self.image, self.rect = load_image("fireball.png", scale=0.3)
+        self.direction = self.getDirection(self.x, self.y)
+
+    def getDirection(self, x, y):
+        if x == 0:
+            return "right"
+        if x == 1450:
+            return "left"
+        if y == 0:
+            return "down"
+        if y == 900:
+            return "up"
+
+    def draw(self, screen):
+        self.hitbox = (self.x + 10, self.y + 10, 28, 10)  # defines the hitbox
+        pg.draw.rect(screen, (255, 0, 0), self.hitbox, 2)
+        screen.blit(self.image, (self.x, self.y)) # not sure why this is so choppy lol
+
 class Player(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)  # call Sprite initializer
-        self.image, self.rect = load_image("player-sprite.gif", scale=0.25)
+        self.image, self.rect = load_image("player-sprite.gif", scale=0.15)
 
     def move_down(self):
         x, y = self.rect.topleft
-        self.rect.topleft = (x, y+10)
+        self.rect.topleft = (x, y+1)
 
     def move_up(self):
         x, y = self.rect.topleft
-        self.rect.topleft = (x, y-10)
+        self.rect.topleft = (x, y-1)
 
     def move_right(self):
         x, y = self.rect.topleft
-        self.rect.topleft = (x+10, y)
+        self.rect.topleft = (x+1, y)
 
     def move_left(self):
         x, y = self.rect.topleft
-        self.rect.topleft = (x-10, y)
+        self.rect.topleft = (x-1, y)
 
 
     def update(self):
