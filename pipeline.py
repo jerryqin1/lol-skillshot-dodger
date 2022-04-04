7f84d72cc1f07793bce669ce1b5089e039a6f128
Nitish Nimma <alphabravo4812@Nitishs-MacBook-Pro.local>
2022-04-04 01:13:16 -0500

    small change to reward

--- pipeline.py
+++ pipeline.py
@@ -242,6 +242,15 @@ class GameState:
                 image_data = pg.surfarray.array3d(pg.display.get_surface())
                 return image_data, -100, True, self.score
 
+            elif 32<= distance < 64:
+                print ("close one!")
+
+                reward =  1/((distance-20)**2) * (10/6)
+
+            else:
+                reward = 10/6
+
+
         # TODO - obs gen
         for event in pg.event.get():
             # generate a new fireball
@@ -275,7 +284,7 @@ class GameState:
 
         image_data = pg.surfarray.array3d(pg.display.get_surface())
         self.clock.tick(FPS)
-        return image_data, (10 / 6), False, self.score
+        return image_data, reward, False, self.score
 
     def get_vel(self, key_direction):
         if key_direction[0] != 0 and key_direction[1] != 0:
