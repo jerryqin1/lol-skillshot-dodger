8bbe88057a48d0fe9c80abc190eb58e7ac1543e7
rong-wang <rong.wang.1@vanderbilt.edu>
2022-04-13 14:43:18 -0500

    Keras RL test

---
+++ kerasrl.py
@@ -0,0 +1,45 @@
+import gym
+import random
+from game_env import GameEnv
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Convolution2D
+from tensorflow.keras.optimizers import Adam
+from rl.agents import DQNAgent
+from rl.memory import SequentialMemory
+from rl.policy import LinearAnnealedPolicy, EpsGreedyQPolicy
+
+
+env = GameEnv()
+height, width, channels = env.observation_space.shape
+actions = env.action_space.n
+
+print('done')
+
+def build_model(height, width, channels, actions):
+    model = Sequential()
+    model.add(Convolution2D(32, (8,8), strides=(4,4), activation='relu', input_shape=(3,height, width, channels)))
+    model.add(Convolution2D(64, (4,4), strides=(2,2), activation='relu'))
+    model.add(Convolution2D(64, (3,3), activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(actions, activation='linear'))
+    return model
+
+model = build_model(height, width, channels, actions)
+
+def build_agent(model, actions):
+    policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.2, nb_steps=10000)
+    memory = SequentialMemory(limit=1000, window_length=3)
+    dqn = DQNAgent(model=model, memory=memory, policy=policy,
+                   enable_dueling_network=True, dueling_type='avg',
+                   nb_actions=actions, nb_steps_warmup=1000
+                   )
+    return dqn
+
+dqn = build_agent(model, actions)
+dqn.compile(Adam(lr=1e-4))
+print('done')
+
+dqn.fit(env, nb_steps=10000, visualize=False, verbose=2)
