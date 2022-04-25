8a19bc4d74d76837531fd4bcf3cf181c4044cb87
Jerry Qin <jerry.gn.qin@gmail.com>
2022-04-25 18:15:51 -0500

    add side by side mode

--- game_env.py
+++ game_env.py
@@ -7,6 +7,7 @@ from pygame.locals import USEREVENT
 import math
 import random
 import cv2
+from pygame.locals import *
 
 WIN_WIDTH = 256
 WIN_HEIGHT = 256
@@ -176,6 +177,7 @@ class GameEnv(gym.Env):
         self.max_obstacles = 4
         self.mintime = 150
         self.maxtime = 200
+        self.score = 0
         # self.mintime = 250
         # self.maxtime = 300
         pg.time.set_timer(USEREVENT + 2,
@@ -250,6 +252,90 @@ class GameEnv(gym.Env):
         self.clock.tick(FPS)
         return x_t, 10, False, {}
 
+    def playGame(self):
+        # Initialise screen
+        self.clock.tick(60)
+        pg.display.set_caption('Skillshot Dodger')
+        font = pg.font.SysFont("comic sans", 40)
+
+        while True:
+            self.score += 1
+
+            player_c_x, player_c_y = self.player.rect.topleft
+            player_c_x += self.player.rect.width / 2
+            player_c_y += self.player.rect.height / 2
+            # player_radius = math.sqrt((player.rect.width / 2) ** 2 +(player.rect.height / 2) ** 2)
+            # player_radius = 34
+
+            for obstacle in self.obstacles:
+                # move the obstacle
+                obstacle.x += obstacle.x_vel
+                obstacle.y += obstacle.y_vel
+
+                fireball_radius = int(obstacle.rect.width / 2)
+                obs_c_x = obstacle.x + fireball_radius
+                obs_c_y = obstacle.y + fireball_radius
+
+                distance = math.dist((player_c_x, player_c_y), (obs_c_x, obs_c_y))
+
+                # if distance < fireball_radius + player_radius:
+                if distance < 32:
+                    print("I got hit")
+                    print("Final score:", self.score)
+                    self.reset()
+
+            for event in pg.event.get():
+                # generate a new fireball
+                if event.type == USEREVENT + 2 and len(self.obstacles) < self.max_obstacles:
+                    self.obstacles.append(Fireball())
+
+                if event.type == QUIT:
+                    return
+                pressed_keys = pg.key.get_pressed()
+
+                key_direction = np.array([0, 0])
+                if pressed_keys[K_LEFT]: key_direction[0] = -5
+                if pressed_keys[K_RIGHT]: key_direction[0] = 5
+                if pressed_keys[K_DOWN]: key_direction[1] = 5
+                if pressed_keys[K_UP]: key_direction[1] = -5
+
+                # key_direction *= dt # ?? - keeps frames consistent but its very fast
+                # TODO: no fractional movement
+                # Idea - keep track of actual position and round before displaying to screen
+                # norm = np.linalg.norm(key_direction)
+                # if norm > 0:
+                #     key_direction = key_direction / norm
+                #     print(key_direction)
+
+                x, y = self.player.rect.topleft
+                x, y = check_bump(x + key_direction[0], y + key_direction[1], 40, 32)
+                self.player.rect.topleft = (x, y)
+
+                # print(key_direction)
+
+            self.allsprites.update()
+            for obstacle in self.obstacles:
+                obstacle.update()
+
+            scoretext = font.render("Score: " + str(self.score), True, (255, 255, 255), (0, 0, 0))
+            self.screen.blit(scoretext, (5, 5))
+
+            # Draw Everything
+            pg.display.update()
+            self.screen.blit(self.background, (0, 0))
+            self.allsprites.draw(self.screen)
+
+            tmp_obstacles = []
+            for obstacle in self.obstacles:
+                if obstacle.x <= -1 or obstacle.y <= -1 or obstacle.x >= WIN_WIDTH + 1 or obstacle.y >= WIN_HEIGHT + 1:
+                    # self.obstacles.pop(self.obstacles.index(obstacle))
+                    continue
+                else:
+                    tmp_obstacles.append(obstacle)
+                    obstacle.draw(self.screen)
+
+            self.obstacles = tmp_obstacles
+
     def reset(self):
         self.obstacles = []
         # pg.time.set_timer(USEREVENT + 2, random.randrange(150, 200))  # determines how often we generate a fireball
@@ -282,3 +368,6 @@ class GameEnv(gym.Env):
                 math.sqrt(self.player.speed) / 2)
         else:
             return key_direction[0] * self.player.speed, key_direction[1] * self.player.speed
+
+    def get_keys_to_action(self):
+        return {(): 1, (276,): 0, (275,): 2, (275, 276): 1}
---
+++ versus.py
@@ -0,0 +1,62 @@
+from kerasrl import build_agent, build_model
+import os
+from game_env import GameEnv
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Convolution2D
+from tensorflow.keras.optimizers import Adam
+from rl.agents import DQNAgent
+from rl.memory import SequentialMemory
+from rl.policy import LinearAnnealedPolicy, EpsGreedyQPolicy
+import multiprocessing
+from gym.utils import play
+
+def _seed(self, seed=None):
+		self.np_random, seed = gym.utils.seeding.np_random(seed)
+		self.robot.np_random = self.np_random # use the same np_randomizer for robot as for env
+		return [seed]
+
+def modelPlay():
+    env = GameEnv()
+    height, width, channels = env.observation_space.shape
+    actions = env.action_space.n
+
+    model = build_model(height, width, channels, actions)
+    dqn = build_agent(model, actions)
+
+    dqn.compile(Adam(learning_rate=1e-4))
+
+    models_dir = "models/"
+    read_file = "dqn_winsize2_6.hdf5"
+    save_file = "dqn_winsize2_7.hdf5"
+
+    if os.path.exists(models_dir + read_file):
+        dqn.load_weights(models_dir + read_file)
+        print("Loaded existing model")
+    else:
+        print("Could not find model at filepath")
+
+    # h = dqn.fit(env, nb_steps=600000, visualize=False, verbose=2)
+    print('done training!')
+
+    # show graph
+    # dqn.save_weights(models_dir + save_file, overwrite=True)
+
+    scores = dqn.test(env, nb_episodes=10, visualize=False)
+    print(np.mean(scores.history['episode_reward']))
+
+
+def humanPlay():
+    env = GameEnv()
+    # play.play(env)
+    env.playGame()
+
+
+
+if __name__ == "__main__":
+    # p1 = multiprocessing.Process(target=modelPlay)
+    # p2 = multiprocessing.Process(target=humanPlay)
+    # #p1.start()
+    # p2.start()
+    humanPlay()
